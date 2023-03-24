07871e052eab3d9a287c46ea392eeb8152beb7c8
Brayan Gibran <brayan.gibran@gmail.com>
2023-03-24 17:06:36 -0600

    Ejercicio de la Capacitación de arquetipo Java

---
+++ office-service/axity.office-persistence/src/main/java/com/axity/office/persistence/OfficePersistence.java
@@ -0,0 +1,18 @@
+package com.axity.office.persistence;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.axity.office.model.OfficeDO;
+
+/**
+ * Persistence interface of  de {@link com.axity.office.model.OfficeDO}
+ * 
+ * @author username@axity.com
+ */
+@Repository
+public interface OfficePersistence extends JpaRepository<OfficeDO, Integer>
+{
+  // Agregar consultas personalizadas
+}
